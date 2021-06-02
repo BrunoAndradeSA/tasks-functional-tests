77c81acb9ef089016c7f68038487955c07af2d6d
Bruno de Andrade Simões de Almeida <bruno.a.s.almeida@gmail.com>
2021-06-02 00:27:37 -0300

    Data futura para os testes

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -13,9 +13,12 @@ public class TasksTest {
 
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 		WebDriver driver = new ChromeDriver();
-		/*DesiredCapabilities cap = DesiredCapabilities.chrome();
-		
-		WebDriver driver = new RemoteWebDriver(new URL("http://172.17.0.1:4444/wd/hub"), cap);*/
+		/*
+		 * DesiredCapabilities cap = DesiredCapabilities.chrome();
+		 * 
+		 * WebDriver driver = new RemoteWebDriver(new
+		 * URL("http://172.17.0.1:4444/wd/hub"), cap);
+		 */
 
 		driver.navigate().to("http://localhost:8088/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -35,7 +38,7 @@ public class TasksTest {
 			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
 
 			// Escrever a data
-			driver.findElement(By.id("dueDate")).sendKeys("01/06/2021");
+			driver.findElement(By.id("dueDate")).sendKeys("01/06/2022");
 
 			// Clicar em 'Save'
 			driver.findElement(By.id("saveButton")).click();
@@ -49,7 +52,7 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
-	
+
 	@Test
 	public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
@@ -73,7 +76,7 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
-	
+
 	@Test
 	public void naoDeveSalvarTarefaSemData() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
@@ -97,7 +100,7 @@ public class TasksTest {
 			driver.quit();
 		}
 	}
-	
+
 	@Test
 	public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
 		WebDriver driver = acessarAplicacao();
